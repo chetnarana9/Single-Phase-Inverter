9657a4c9de3ba2844357d23684a6e645922ffa7b
Chetna <ranajichetu013@gmail.com>
2025-12-23 17:52:20 +0530

    Add files via upload

---
+++ single_pahse_inverter.ino
@@ -0,0 +1,68 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+
+  // Signal to PIN 8
+  digitalWrite(8, HIGH);
+  delayMicroseconds(500);
+  digitalWrite(8, LOW);
+  delayMicroseconds(500);
+  digitalWrite(8, HIGH);
+  delayMicroseconds(750);
+  digitalWrite(8, LOW);
+  delayMicroseconds(500);
+  digitalWrite(8, HIGH);
+  delayMicroseconds(1250);
+  digitalWrite(8, LOW);
+  delayMicroseconds(500);
+  digitalWrite(8, HIGH);
+  delayMicroseconds(2000);
+  digitalWrite(8, LOW);
+  delayMicroseconds(500);
+  digitalWrite(8, HIGH);
+  delayMicroseconds(1250);
+  digitalWrite(8, LOW);
+  delayMicroseconds(500);
+  digitalWrite(8, HIGH);
+  delayMicroseconds(750);
+  digitalWrite(8, LOW);
+  delayMicroseconds(500);
+  digitalWrite(8, HIGH);
+  delayMicroseconds(500);
+  digitalWrite(8, LOW);
+  delayMicroseconds(500);
+
+  // Signal to PIN 9
+  digitalWrite(9, HIGH);
+  delayMicroseconds(500);
+  digitalWrite(9, LOW);
+  delayMicroseconds(500);
+  digitalWrite(9, HIGH);
+  delayMicroseconds(750);
+  digitalWrite(9, LOW);
+  delayMicroseconds(500);
+  digitalWrite(9, HIGH);
+  delayMicroseconds(1250);
+  digitalWrite(9, LOW);
+  delayMicroseconds(500);
+  digitalWrite(9, HIGH);
+  delayMicroseconds(2000);
+  digitalWrite(9, LOW);
+  delayMicroseconds(500);
+  digitalWrite(9, HIGH);
+  delayMicroseconds(1250);
+  digitalWrite(9, LOW);
+  delayMicroseconds(500);
+  digitalWrite(9, HIGH);
+  delayMicroseconds(750);
+  digitalWrite(9, LOW);
+  delayMicroseconds(500);
+  digitalWrite(9, HIGH);
+  delayMicroseconds(500);
+  digitalWrite(9, LOW);
+  delayMicroseconds(500);
+}
